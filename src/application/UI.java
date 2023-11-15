0c2f48ea3079f0fa05275f0911ddac55b7f6e337
relESCampONes <wellington_lima@live.com>
2023-11-15 19:49:12 -0300

    Little improvement in board printing

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,8 +1,32 @@
 package application;
 
 import chess.ChessPiece;
+import chess.Color;
 
 public class UI {
+	// Reset
+	public static final String RESET = "\033[0m"; // Text Reset
+
+	// Bold
+	public static final String BLACK_BOLD = "\033[1;30m"; // BLACK
+	public static final String RED_BOLD = "\033[1;31m"; // RED
+	public static final String GREEN_BOLD = "\033[1;32m"; // GREEN
+	public static final String YELLOW_BOLD = "\033[1;33m"; // YELLOW
+	public static final String BLUE_BOLD = "\033[1;34m"; // BLUE
+	public static final String PURPLE_BOLD = "\033[1;35m"; // PURPLE
+	public static final String CYAN_BOLD = "\033[1;36m"; // CYAN
+	public static final String WHITE_BOLD = "\033[1;37m"; // WHITE
+
+	// Background
+	public static final String BLACK_BACKGROUND = "\033[40m"; // BLACK
+	public static final String RED_BACKGROUND = "\033[41m"; // RED
+	public static final String GREEN_BACKGROUND = "\033[42m"; // GREEN
+	public static final String YELLOW_BACKGROUND = "\033[43m"; // YELLOW
+	public static final String BLUE_BACKGROUND = "\033[44m"; // BLUE
+	public static final String PURPLE_BACKGROUND = "\033[45m"; // PURPLE
+	public static final String CYAN_BACKGROUND = "\033[46m"; // CYAN
+	public static final String WHITE_BACKGROUND = "\033[47m"; // WHITE
+
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
@@ -18,7 +42,11 @@ public class UI {
 		if (piece == null) {
 			System.out.print("-");
 		} else {
-			System.out.print(piece);
+			if (piece.getColor() == Color.WHITE) {
+				System.out.print(WHITE_BOLD + piece + RESET);
+			} else {
+				System.out.print(YELLOW_BOLD + piece + RESET);
+			}
 		}
 		System.out.print(" ");
 	}
